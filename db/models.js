1f641da9d1cf7aab4884926b3550786fe449d230
pirateMagoo <williammckenzie275@gmail.com>
2024-05-29 11:34:25 +0100

    this commit removes a console.log i neglected to remove from last commit

--- db/models.js
+++ db/models.js
@@ -22,9 +22,9 @@ function fetchArticleById(article_id) {
     return db.query(`SELECT * FROM articles WHERE article_id = $1`,
         [article_id])
         .then(({rows}) => {
-            console.log(rows)
             return rows[0]
         })
+            
 };
 
 
